687069ea8adeccb9db48de6a53a519101de298c9
Do-yeong Choi <han030111@kakao.net>
2024-12-26 23:24:58 +0900

    1

--- time/main.js
+++ time/main.js
@@ -1,18 +1,22 @@
-setInterval (aa,1000)
-let cri=new Date("2024-12-24:00:00:00+0900")
-let today=new Date ()
-let sec= (cri-today)/1000
-let span=document.querySelector("span")
+
 let a=document.querySelector(".a")
 let b=document.querySelector(".b")
+let span=document.querySelector("span")
 let c=document.querySelector(".c")
-function aa(){
-  sec--
-  day=Math.round(sec/86400)
-  hour=Math.round(sec%86400/3600)
-  min=Math.round(sec%86400%3600/60)
-  span.textContent=day
-  a.textContent=hour
-  b.textContent=min
-  c.textContent=Math.round(sec%60)
+let d=document.querySelector(".d")
+let cri = new Date("2024-12-25:00:00:00+0900");
+let now=new Date()
+console.log(cri)
+let sec=(cri-now)/1000
+
+function aa(){ 
+sec--
+Day=Math.floor(sec/86400)
+hour=Math.floor(sec%86400/3600)
+min=Math.floor(sec%86400%3600/60)
+span.textContent=Day
+a.textContent=hour
+b.textContent=min
+c.textContent=Math.floor(sec%60)
 }
+setInterval(aa,1000)
