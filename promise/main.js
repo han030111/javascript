11ea4331a8c7fe4037c1b205ff7e11dc9a73b840
Do-yeong Choi <han030111@kakao.net>
2025-01-21 19:38:33 +0900

    1

--- promise/main.js
+++ promise/main.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (request, response) => {
-	response.send("안녕하세요.");
-});
-
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+const ex= require("express")
+const app=ex()
+const port=300
+app.get("/",(req,res)=>{
+  res.send("hello world")
+})
+app.listen(port,()=>{
+  console.log(
+    "exemple${port}"
+  )
+})
